cb093e5e590101f6c93bd69137a0bc6e3ba1d937
Satori32 <Huga8989+githubf@gmail.com>
2023-10-26 22:02:56 +0900

    add some test

--- ソース.cpp
+++ ソース.cpp
@@ -6,6 +6,8 @@
 #include <limits>
 #include <random>
 
+//not debug now
+
 typedef double Float;
 
 Float Sin(Float T) {
@@ -25,57 +27,78 @@ Float SawBi(Float T) {
 	return 0.0 - (T * 1);
 }
 Float SawC(Float T) {
-	return (2.0 - (T * 1))-1;
+	return (2.0 - (T * 2)) - 1;
 }
 Float SawCi(Float T) {
-	return (-2.0 + (T * 1))+1;
+	return (-2.0 + (T * 2)) + 1;
 }
 Float SquareA(Float T) {
-	return 1*T;
+	return 1;
 }
 Float SquareAi(Float T) {
-	return -1*T;
+	return -1;
 }
 Float SquareB(Float T) {
 	return T>0? 1:-1;
 }
-Float Triangle(Float T) {
+Float TriangleA(Float T) {
 
-	Float X = (2.0 * T)-1;
-	if (T >= 0.5) X = (-2.0 * T) + 1;
+	Float X = (2.0 * T);
+	if (T >= 0.5) X = 2.0-(2.0 * T);
+
+	return X;
+}
+Float TriangleAi(Float T) {
+
+	Float X = (-2.0 * T);
+	if (T >= 0.5) X = -2.0+(2.0 * T);
+
+	return X;
+}Float TriangleB(Float T) {
+
+	Float X = -1.0+(4.0 * T);
+	if (T > 0.5) X = 1.0-(4.0 * (T-0.5));
+
+	return X;
+}
+Float TriangleBi(Float T) {
+
+	Float X = 1.0-(4.0 * T);
+	if (T > 0.5) X = -1.0+(4.0 * (T-0.5));
 
 	return X;
 }
 Float TrapezoidA(Float T) {
 
-	Float X = T*4;
+	Float X = -1.0+T * 8;
 	if (T >= 0.25) X = 1;
-	if (T >= 0.75) X = 1 - ( (T - 0.5) * 4);
+	if (T > 0.75) X = 1.0 - ( (T - 0.75) * 8);
+
 
 	return X;
 }
 Float TrapezoidAi(Float T) {
 	
-	Float X = -T*4;
+	Float X = 1.0 - T * 8;
 	if (T >= 0.25) X = -1;
-	if (T >= 0.75) X = -1 + ((T - 0.5) * 4);
+	if (T > 0.75) X = -1.0 + ((T - 0.75) * 8);
 
 	return X;
 
 }
 Float TrapezoidB(Float T) {
 	
-	Float X = (-1 + T * 8);
+	Float X = (T * 4);
 	if (T >= 0.25) X = 1;
-	if (T >= 0.75) X = 1 - ((T - 0.5)  * 8);
+	if (T > 0.75) X = 1 - ((T - 0.75)  * 4);
 
 	return X;
 }
 Float TrapezoidBi(Float T) {
 	
-	Float X = (1 - T * 8);
+	Float X = -( T * 4);
 	if (T >= 0.25) X = -1;
-	if (T >= 0.75) X = 1 - ((T - 0.5) * 8);
+	if (T > 0.75) X = -1+((T - 0.75) * 4);
 
 	return X;
 }
@@ -85,4 +108,31 @@ Float FM_One(Float T,Float F1,Float F2, std::uint32_t Ch) {
 	Float R = Sin(F1 * T + PI / 2 * Sin(F2 * T));
 
 	return R;
+}
+
+std::vector<Float> Loop(Float(*Fp)(Float), std::uintmax_t N) {
+	std::vector<Float> R;
+
+	for (std::uintmax_t i = 0; i < N; i++) {
+		R.push_back(Fp(1.0 / 360 * i));
+	}
+
+	return R;
+}
+
+int main() {
+
+	auto A = Loop(SawA, 360);
+	auto B = Loop(SawAi, 360);
+	auto C = Loop(SawC, 360);
+	auto D = Loop(SawCi, 360);
+	auto E = Loop(TriangleA, 360);
+	auto F = Loop(TriangleAi, 360);
+	auto G = Loop(TriangleB, 360);
+	auto H = Loop(TriangleBi, 360);
+	auto I = Loop(TrapezoidA, 360);
+	auto J = Loop(TrapezoidAi, 360);
+	auto K = Loop(TrapezoidB, 360);
+	auto L = Loop(TrapezoidBi, 360);
+	return 0;
 }
